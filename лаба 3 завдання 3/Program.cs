78d04501c4ce93952fefcad59957b2972674620e
DashaShubina18 <shubinad268@gmail.com>
2024-10-05 17:38:38 +0300

    Update Program.cs

--- лаба 3 завдання 3/Program.cs
+++ лаба 3 завдання 3/Program.cs
@@ -14,14 +14,18 @@ namespace лаба_3_завдання_3
             double b = 10.5; //кінець проміжку
             double h = 0.2; //відступ 
             double e = 2.72;
-            Console.WriteLine("x\t\ty\t\tz"); //заголовок таблиці з табуляціями
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("|         x        |            y        |         z       |"); //заголовок таблиці з табуляціями
+            Console.WriteLine("------------------------------------------------------------");
+           
             for (double x = a; x <= b; x += h)
             {
                 double y = Math.Cos(1.5 * x) * Math.Log10(2.5 * x);
                 double z = Math.Pow(e, 1 / Math.Sqrt(x)) * Math.Sin(x);
 
-                Console.WriteLine($"{x:F2}\t\t {y:F6}\t\t {z:F6}");
+                 Console.WriteLine($"|   {x,6:F2}         |   {y,12:F6}      |    {z,10:F6}   |");
             }
+            Console.WriteLine("-------------------------------------------------------------");
             Console.ReadKey();
 
 
